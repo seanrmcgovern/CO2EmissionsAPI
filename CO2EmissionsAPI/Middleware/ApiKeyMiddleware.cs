9f7a1c50af195aea6461abb2f42d0f45a99141a9
Sean McGovern <srm5ka@virginia.edu>
2024-11-03 21:56:30 -0500

    Added middleware for api key

---
+++ CO2EmissionsAPI/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,39 @@
+﻿
+namespace CO2EmissionsAPI.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEYHEADERNAME = "x-api-key";
+        private const string APIKEYSETTINGSNAME = "ApiKey";
+
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEYHEADERNAME, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key was not provided.");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = appSettings.GetValue<string>(APIKEYSETTINGSNAME);
+
+            if (!apiKey.Equals(extractedApiKey.FirstOrDefault()))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
+
